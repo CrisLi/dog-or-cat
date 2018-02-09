c15db2e34bd5ce3dccee87cde7a77c04431fa031
Chris Li <chrisandkitty@163.com>
2018-02-09 14:52:43 +0800

    Add basic handler

--- components/Vote.js
+++ components/Vote.js
@@ -1,9 +1,9 @@
-export default ({ count, onClick, message }) => (
+export default ({ count, vote, message, name }) => (
   <div className="card border-primary" style={{ width: '20rem' }}>
     <div className="card-header">Vote: {count}</div>
     <div className="card-body text-primary">
       <h4 className="card-title">{message}</h4>
-      <button className="btn btn-primary btn-block" onClick={onClick}>Vote</button>
+      <button className="btn btn-primary btn-block" onClick={() => vote(name)}>Vote</button>
     </div>
   </div>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
+import { compose, withState, withHandlers } from 'recompose';
 import Greeting from 'components/Greeting';
 import Vote from 'components/Vote';
 
-export default () => (
+const withWholeVotes = compose(
+  withState('dogs', 'voteDog', 0),
+  withState('cats', 'voteCat', 0),
+  withHandlers({
+    vote: ({ voteDog, voteCat }) => (to) => {
+      switch (to) {
+        case 'dogs':
+          voteDog((current) => current + 1);
+          break;
+        case 'cats':
+          voteCat((current) => current + 1);
+          break;
+        default:
+          break;
+      }
+    }
+  })
+);
+
+const Index = ({ dogs, cats, vote }) => (
   <div className="container">
     <div className="main">
       <div className="d-flex justify-content-center">
@@ -9,10 +29,10 @@ export default () => (
       </div>
       <div className="row">
         <div className="col-sm d-flex justify-content-center">
-          <Vote count={10} message="I like Dog" />
+          <Vote count={dogs} message="I like Dog" name="dogs" vote={vote} />
         </div>
         <div className="col-sm d-flex justify-content-center">
-          <Vote count={10} message="I like Cat" />
+          <Vote count={cats} message="I like Cat" name="cats" vote={vote} />
         </div>
       </div>
     </div>
@@ -24,3 +44,5 @@ export default () => (
     </style>
   </div>
 );
+
+export default withWholeVotes(Index);
